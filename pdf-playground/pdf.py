c52058cdc9285c3ec8ac9ea239a6bbdea6c535a0
boodscode237 <boodscode@gmail.com>
2020-09-25 16:14:33 +0500

    mark page programm

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,5 +1,55 @@
 import PyPDF2
 
+template = PyPDF2.PdfFileReader(open('super.pdf','rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf','rb'))
+
+outputs = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+	page = template.getPage(i)
+	page.mergePage(watermark.getPage(0))
+	outputs.addPage(page)
+
+	with open('watermarked_output.pdf', 'wb') as file:
+		outputs.write(file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+a program to merge many pdf files
+'''
+
+import sys
+
+inputs = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+	merger = PyPDF2.PdfFileMerger()
+	for pdf in pdf_list:
+		print(pdf)
+		merger.append(pdf)
+	merger.write('super.pdf')
+pdf_combiner(inputs)
+
+
+
+'''
+How to grab a file . read it(rb read binary)
+rotate the page, and write a new page
+
+'''
+
+
 with open('dummy.pdf', 'rb') as file:
 	reader = PyPDF2.PdfFileReader(file)
 	page = reader.getPage(0)
